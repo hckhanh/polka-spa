577462ddf6cc429fbf6a61c762f3e54fd6d54e9c
Khánh Hoàng <hoangchaukhanh93@gmail.com>
2020-06-10 07:27:48 +0700

    feat: initial project

---
+++ configs/helmet.js
@@ -0,0 +1,17 @@
+const yaml = require("js-yaml");
+const fs = require("fs");
+
+const csp = yaml.safeLoad(fs.readFileSync("./csp.yml", "utf8"));
+csp.directives.scriptSrc = process.env.CSP_SCRIPT_SRC || "'self'";
+
+if (process.env.CSP_SCRIPT_SRC_ELEM) {
+  csp.directives.scriptSrcElem = process.env.CSP_SCRIPT_SRC_ELEM;
+}
+
+csp.directives.connectSrc = process.env.CSP_CONNECT_SRC || "'self'";
+
+module.exports = {
+  enableCsp: process.env.ENABLE_CSP && Boolean(process.env.ENABLE_CSP),
+  referrerPolicy: process.env.REFERRER_POLICY || "same-origin",
+  csp
+};
